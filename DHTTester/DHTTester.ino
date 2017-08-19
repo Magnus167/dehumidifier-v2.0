08033a833facb0f1b43758b36c2c8da245d08ad9
Palash Tyagi <palashtyagi167@gmail.com>
2017-08-19 09:45:23 +0530

    Add files via upload

---
+++ DHTTester/DHTTester.ino
@@ -0,0 +1,42 @@
+#include <SoftwareSerial.h>
+
+// Example testing sketch for various DHT humidity/temperature sensors
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHT22 test!");
+ 
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C ");
+}
---
+++ Main/Main.ino
@@ -0,0 +1,104 @@
+/*
+  Adafruit Arduino - Lesson 4. 8 LEDs and a Shift Register
+*/
+
+#include "DHT.h"
+
+#define DHTPIN 3     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+int latchPin = 1;
+int clockPin = 2;
+int dataPin = 0;
+
+byte leds = 0;
+
+void setup()
+{
+  //Serial.begin(9600);
+  //Serial.println("TEST");
+  dht.begin();
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  leds = 0;
+  updateShiftRegister();
+}
+
+void loop()
+{
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  //Serial.print("Humidity: ");
+  //Serial.print(h);
+  //Serial.print(" %\t");
+  //Serial.print("Temperature: ");
+  //Serial.print(t);
+  //Serial.println(" *C ");
+
+
+  leds = 0;
+
+
+
+  if (isnan(h) || isnan(t)) {
+    bitSet(leds, 2);
+    bitSet(leds, 3);
+    bitClear(leds, 5);
+    bitClear(leds, 6);
+    bitSet(leds, 4);
+    bitClear(leds, 7);
+    updateShiftRegister();
+    return;
+  }
+  else if ((t >= 35) && (h <= 25))
+  {
+    bitClear(leds, 2);  // everything GOOD --> GREEN & fan ON
+    bitSet(leds, 3);
+    bitSet(leds, 5);
+    bitClear(leds, 6);
+    bitClear(leds, 4);
+    bitClear(leds, 7);
+    updateShiftRegister();
+  }
+  else if (t < 35)
+  {
+    bitSet(leds, 2);  //temp. bad ----> show red
+    bitClear(leds, 3);
+    bitSet(leds, 5);
+    bitSet(leds, 6);
+    bitClear(leds, 4);
+    bitClear(leds, 7);
+    updateShiftRegister();
+  }
+  else if (h > 25)
+  {
+    bitSet(leds, 4);  //humidity bad ----> show blue
+    bitClear(leds, 3);
+    bitSet(leds, 5);
+    bitClear(leds, 6);
+    bitClear(leds, 2);
+    bitClear(leds, 7);
+    updateShiftRegister();
+  }
+
+
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  delay(5000);
+
+
+}
+
+void updateShiftRegister()
+{
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
+}
